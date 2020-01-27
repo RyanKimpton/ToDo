20266eceffd754992d28868788c8a5433f6f9a59
Ryans mac <RyanKimpton965@gmail.com>
2020-01-27 11:22:49 +0000

    Added a button that will add new notes along with a lot of changes to allow this to show

--- App/Views/Home.js
+++ App/Views/Home.js
@@ -2,23 +2,39 @@ import React from '../../node_modules/react';
 import {StyleSheet, Text, View} from '../../node_modules/react-native/Libraries/react-native/react-native-implementation';
 import {Header} from '../sections/Header.js';
 import {Menu} from '../sections/Menu.js';
+import AddButton from '../utils/AddButton.js'
+
+import { FAB } from 'react-native-paper';
+
+
+
 
 export class Home extends React.Component{
     static navigationOptions = {
         headerShown: false
     };
+
+    
     
     render() {
         const {navigate} = this.props.navigation;
 
         const user = this.props.navigation.state.params.username;
 
+
+
         return (
             <View style = {styles.container}>
                 <Header message = {user} />
-                
-                <View style = {styles.middle}>
 
+                <View style = {styles.middle}>
+                    <FAB
+                        style={styles.fab}
+                        small
+                        icon="plus"
+                        color = '#008080'
+                        onPress={() => console.log('Pressed')}
+                    />
                 </View>
 
                 <Menu navigate = {navigate} />
@@ -33,6 +49,12 @@ const styles = StyleSheet.create({
     },
     middle: {
         flex: 15
-    }
+    },
+    fab: {
+        position: 'absolute',
+        margin: 16,
+        right: 0,
+        bottom: 0,
+      },
     
 })
--- App/sections/Header.js
+++ App/sections/Header.js
@@ -1,5 +1,6 @@
 import React from '../../node_modules/react';
 import {StyleSheet, Text, View, Image} from '../../node_modules/react-native';
+import { withNavigation } from 'react-navigation';
 
 export class Header extends React.Component {
     constructor(props) {
@@ -7,23 +8,36 @@ export class Header extends React.Component {
         this.state = {isLoggedIn : false};
     }
 
-    toggleUser = () => {
-        this.setState(previousState => {
-            return { isLoggedIn: !previousState.isLoggedIn};
-        });
+    static navigationOptions = {
+        headerShown: false
+      };
+
+    logout() {
+        console.log("hello");
+        console.log(this.props);
+
+        this.props.navigation.navigate('LandingRoute');
+
+        Alert.alert(`Goodbye ${username}`);
     }
 
     render() {
-        let display = this.state.isLoggedIn ? 'sample User' : this.props.message;
+        let name = this.state.isLoggedIn ? 'sample User' : this.props.message;
         return (
             <View style = {styles.headStyle}>
-                <Text onPress = {this.toggleUser} style = {styles.headText}>
-                    {display}
+                <Text onPress = {this.logout} style = {styles.logoutText}>
+                    Logout
+                </Text>
+
+                <Text style = {styles.headText}>
+                    {name}'s todo list
                 </Text>
             </View>
         )
     }
 }
+
+export default withNavigation(Header);
 
 const styles = StyleSheet.create({
     headText: {
@@ -32,10 +46,16 @@ const styles = StyleSheet.create({
         fontSize: 20,
         flex: 1
     },
+    logoutText: {
+        textAlign: 'left',
+        color: '#ffffff',
+        fontSize: 20,
+        flex: 1
+    },
     headStyle: {
         paddingTop: 30,
         paddingRight: 10,
-        backgroundColor: '#35605a',
+        backgroundColor: '#008080',
         flex: 1,
         borderBottomWidth: 2,
         borderColor: '#000000'
